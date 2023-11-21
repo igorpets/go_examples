f6074cb563aae9e5b49f7e9c25b0d7de5a97b11e
Igor Pets <b_ear@mail.ru>
2023-11-21 23:47:10 +0300

    test5

--- test.go
+++ test.go
@@ -10,4 +10,9 @@ func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(77))
 	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
+}
+
+func add(x int, y int) int {
+	return x + y
 }
